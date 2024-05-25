52cfca5b194312eb78b8df889063c76f528e447e
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2024-05-26 01:20:17 +0500

    add footer

--- web/src/components/footer/Footer.jsx
+++ web/src/components/footer/Footer.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
-import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer className="d-flex justify-content-evenly align-items-center mt-5 footer" >
-      <div className="">
-        <a href="https://www.facebook.com"className="px-2" ><FaFacebookF /></a>
-        <a href="https://www.twitter.com" className="px-2"><FaTwitter /></a>
-        <a href="https://www.instagram.com" className="px-2"><FaInstagram /></a>
+    <footer style={styles.footer}>
+      <div style={styles.social}>
+        <a href="https://www.facebook.com" style={styles.icon}><FaFacebookF /></a>
+        <a href="https://www.twitter.com" style={styles.icon}><FaTwitter /></a>
+        <a href="https://www.instagram.com" style={styles.icon}><FaInstagram /></a>
       </div>
-      <div>
-      <p className="mt-3">&copy; 2024 Your Website</p>
-      </div>
+      <p style={styles.copyright}>&copy; 2024 Your Website</p>
     </footer>
   );
 };
 
+const styles = {
+  footer: {
+    backgroundColor: '#ffc107',
+    color: 'grey',
+    padding: '20px',
+    textAlign: 'center',
+    position: 'fixed',
+    bottom: '0',
+    width: '100%',
+  },
+  social: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '10px',
+  },
+  icon: {
+    fontSize: '24px',
+    color: 'grey',
+    margin: '0 10px',
+    textDecoration: 'none',
+  },
+  copyright: {
+    fontSize: '14px',
+  },
+};
 
-export default Footer;
+export default Footer;
